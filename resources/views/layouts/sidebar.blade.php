7a58edcc49357cb9657820223bb11885457e972b
Gheanur <bunabun29@gmail.com>
2025-05-07 09:42:20 +0700

    k

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,18 +1,9 @@
 
 <div class="d-flex flex-column p-3 bg-light text-black" style="height: 100vh;">
-    <h4 class="mb-4">InstaClone</h4>
-
-    <a href="{{ route('home') }}" class="btn btn-success text-black text-decoration-none mb-3 {{ request()->routeIs('home') ? 'fw-bold' : '' }}">
-        Home
-    </a>
-
+    <h4 class="mb-4">Tambah barang</h4>
     <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createPostModal">
         Tambah Barang
       </button>
-
-    <a href="{{ route('profile') }}" class="btn btn-success text-black text-decoration-none mb-3 {{ request()->routeIs('profile') ? 'fw-bold' : '' }}">
-        Profile
-    </a>
 </div>
 
 
